140a31cad3da8c63a4c12b9c143c5596e9f2bdf8
Paul Downey <psd@whatfettle.com>
2018-06-18 15:10:41 +0100

    Incrementally canonicalise geojson

--- lib/geojson.py
+++ lib/geojson.py
@@ -1,92 +1,22 @@
 #!/usr/bin/env python3
 
+"""
+    simplify geojson output by ogr2ogr GeoJSON driver
+    - limits coordinates to 6 decimal places
+
+    Assumes coordinates are in WGS84
+"""
+
+try:
+    import ijson.backends.yajl2_cffi as ijson
+except ImportError:
+    import ijson
+
 import json
 import sys
 import os
 from decimal import Decimal
-from pyproj import Proj, transform
 
-projection = {
-    'bng': Proj(init='EPSG:27700'),
-    'wgs84': Proj(init='EPSG:4326')
-}
-
-count = 0
-
-def c14n(geojson, prefix, key):
-
-    def coordinates(c):
-
-        return [round(Decimal(c[0]), 5), round(Decimal(c[1]), 5)]
-
-    def point(g):
-        return {
-            "type": "Point",
-            "coordinates": coordinates(g['coordinates'])
-        }
-
-    def polygon(g):
-        return {
-            "type": "Polygon",
-            "coordinates": [[coordinates(c) for c in l] for l in g['coordinates']]
-        }
-
-    def multipolygon(g):
-        return {
-            "type": "MultiPolygon",
-            "coordinates": [[[coordinates(c) for c in l] for l in ll] for ll in g['coordinates']]
-        }
-
-    def geometry(g):
-        if g['type'] == 'Point':
-            return point(g)
-        elif g['type'] == 'Polygon':
-            return polygon(g)
-        elif g['type'] == 'MultiPolygon':
-            return multipolygon(g)
-        else:
-            raise ValueError('invald geometry', g['type'])
-
-    def properties(p):
-        global count
-        if key and key in p:
-            f="%s:%s" % (prefix, p[key])
-        else:
-            f=count
-            count = count + 1
-        return { 'feature': f }
-
-    def feature(f):
-
-        print(f)
-
-        if f['type'] != 'Feature':
-            raise ValueError('invald feature', f['type'])
-
-        return {
-            "type": "Feature",
-            "properties": properties(f['properties']),
-            "geometry": geometry(f['geometry'])
-        }
-
-    def collection(c):
-
-        if c['type'] != 'FeatureCollection':
-            raise ValueError('invalid geojson')
-        return {
-            "type": "FeatureCollection",
-            "features": [feature(f) for f in c['features'] if f.get('geometry') is not None]
-        }
-
-    return collection(geojson)
-
-
-def dump(geojson, output):
-    json.dump(geojson, output, default=decimal_default, separators=(',', ':'), sort_keys=True)
-
-
-def load(i):
-    return json.load(i)
 
 def decimal_default(obj):
     if isinstance(obj, Decimal):
@@ -94,7 +24,76 @@ def decimal_default(obj):
     raise TypeError
 
 
+def coordinates(c):
+    return [round(Decimal(c[0]), 5), round(Decimal(c[1]), 5)]
+
+
+def point(g):
+    return {
+        "type": "Point",
+        "coordinates": coordinates(g['coordinates'])
+    }
+
+
+def polygon(g):
+    return {
+        "type": "MultiPolygon",
+        "coordinates": [[[coordinates(c) for c in l] for l in g['coordinates']]]
+    }
+
+
+def multipolygon(g):
+    return {
+        "type": "MultiPolygon",
+        "coordinates": [[[coordinates(c) for c in l] for l in ll] for ll in g['coordinates']]
+    }
+
+
+def geometry(g):
+    if g['type'] == 'Point':
+        return point(g)
+    elif g['type'] == 'Polygon':
+        return polygon(g)
+    elif g['type'] == 'MultiPolygon':
+            return multipolygon(g)
+    else:
+        raise ValueError('invald geometry', g['type'])
+
+
+def properties(p, prefix, key):
+    if key and key in p:
+        f="%s:%s" % (prefix, p[key])
+        return { 'feature': f }
+    else:
+        return {}
+
+
+def feature(f, prefix, key):
+    if f['type'] != 'Feature':
+        raise ValueError('invald feature', f['type'])
+
+    return {
+        "type": "Feature",
+        "properties": properties(f['properties'], prefix, key),
+        "geometry": geometry(f['geometry'])
+    }
+
+
+def dump(obj, file):
+    json.dump(obj, file, default=decimal_default, separators=(',', ':'), sort_keys=True)
+
+
+def c14n(prefix, key, ifp=sys.stdin, ofp=sys.stdout):
+    print('{ "type": "FeatureCollection", "features": [', file=ofp)
+
+    features = ijson.items(ifp, 'features.item')
+    for f in features:
+        if 'geometry' in f and f['geometry']:
+            dump(feature(f, prefix, key), file=ofp)
+            print(file=ofp)
+
+    print(']}', file=ofp)
+
+
 if __name__ == "__main__":
-    geojson = load(sys.stdin)
-    geojson = c14n(geojson, prefix=sys.argv[1], key=sys.argv[2])
-    dump(geojson, sys.stdout)
+    c14n(prefix=sys.argv[1], key=sys.argv[2])
